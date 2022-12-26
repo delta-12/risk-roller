ff9b25032a8a8e44fc7a99dea7123bd5f09149a9
delta-12 <btesq235@gmail.com>
2022-12-26 12:18:12 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function rollDie() {
+  return Math.floor(Math.random() * 6) + 1
 }
 
-export default App;
+function combinedRoll(dice, player) {
+  let max = (player === "attacker") ? 3 : 2
+  let rolls = 0
+  let results = []
+  if (dice >= max) {
+    rolls = max
+  }
+  else {
+    rolls = dice
+  }
+  for (let i = 0; i < rolls; i++) {
+    results.push(rollDie())
+  }
+  results = results.sort().reverse()
+  console.log(results)
+  return results
+}
+
+function rollDice(a, d) {
+  console.log("ROLLING...")
+  let attacker = a
+  let defender = d
+  let winner, troops
+  let rollCount = 0
+  while (attacker > 0 && defender > 0) {
+    rollCount++
+    console.log("Roll", rollCount)
+    let attackerRoll = combinedRoll(attacker, "attacker")
+    let defenderRoll = combinedRoll(defender, "defender")
+    let pairs = Math.min(attackerRoll.length, defenderRoll.length)
+    for (let i = 0; i < pairs; i++) {
+      if (attackerRoll[i] > defenderRoll[i]) {
+        defender--
+      }
+      else {
+        attacker--
+      }
+    }
+  }
+  if (attacker === 0 && defender === 0) {
+    winner = "error"
+    troops = -1
+  }
+  else {
+    winner = (attacker > defender) ? "Attacker" : "Defender"
+    troops = (attacker > defender) ? attacker : defender
+  }
+  return { winner, troops }
+}
+
+export default class App extends Component {
+  state = {
+    attacker: 0,
+    defender: 0,
+    winner: "",
+    troops: 0
+  }
+
+  onAttackerChange = e => {
+    this.setState({
+      attacker: e.target.value
+    })
+  }
+
+  onDefenderChange = e => {
+    this.setState({
+      defender: e.target.value
+    })
+  }
+
+  roll = e => {
+    e.preventDefault()
+    let results = rollDice(this.state.attacker, this.state.defender)
+    console.log(results)
+    this.setState({
+      winner: results.winner,
+      troops: results.troops
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Risk Roller</h1>
+          <p>Attacker</p>
+          <input type="title" onChange={this.onAttackerChange} />
+          <p>Defender</p>
+          <input type="title" onChange={this.onDefenderChange} />
+          <br />
+          <button onClick={this.roll}>Roll</button>
+          <br />
+          {(this.state.winner.length > 0) ? <p>Winner: {this.state.winner}  |  Troops: {this.state.troops} </p> : <p></p>}
+        </header>
+      </div>
+    )
+  }
+}
